1ba1af1e1f80bffd80d7d861c5244afa3a588cb0
VibolSoursey <160739021+VibolSoursey@users.noreply.github.com>
2024-02-22 08:35:17 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace character
+
+
+public class Main {
+    public static void main(String[] args) {
+        Array myArray = new Array();
+
+      }
+}
